74793d38d55b5735257d3d931a3330add14da87c
MadHapps <115760696+MadHapps@users.noreply.github.com>
2024-04-29 15:17:59 -0700

    Removed game.css and Game.jsx and refactored App.jsx/css to include it

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import './App.css'
-import Game from './components/Game'
+import { useState } from "react";
+import "./App.css";
+import Die from "./components/Die";
 
 function App() {
+  const randomDieValueArray = () => Array.from({ length: 10 }, () => Math.floor(Math.random() * 6) + 1)
+  const [dice, setDice] = useState(randomDieValueArray)
+  const diceElements = dice.map((dieValue, index) => <Die key={index} value={dieValue} /> )
+
+  function roll() {
+    setDice(randomDieValueArray)
+  }
 
   return (
     <main>
-      <section className='title'>
+      <section className="title">
         <h1>Tenzies</h1>
-        <h2>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h2>
+        <h2>
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </h2>
       </section>
-      <Game />
+      <ul className="die-wrapper">
+        {diceElements}
+      </ul>
+      <button onClick={roll}>Roll</button>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
