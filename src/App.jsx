1a893fcdda8711b45f2de498cefee46c514496cb
MadHapps <115760696+MadHapps@users.noreply.github.com>
2024-05-01 15:45:18 -0700

    Added confetti animation and New Game option on win

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Die from "./components/Die";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 function App() {
   const [game, setGame] = useState({ clicks: 0, won: false });
@@ -33,45 +34,48 @@ function App() {
   }
 
   function roll() {
-    setGame((prevGame) => ({
-      ...prevGame,
-      clicks: prevGame.clicks + 1,
-    }));
+    if (!game.won) {
+      setGame((prevGame) => ({
+        ...prevGame,
+        clicks: prevGame.clicks + 1,
+      }));
 
-    setDice((prevDice) =>
-      prevDice.map((die) =>
-        die.isHeld
-          ? { ...die }
-          : { ...die, value: Math.ceil(Math.random() * 6) }
-      )
-    );
+      setDice((prevDice) =>
+        prevDice.map((die) =>
+          die.isHeld
+            ? { ...die }
+            : { ...die, value: Math.ceil(Math.random() * 6) }
+        )
+      );
+    } else {
+      setGame({ clicks: 0, won: false })
+      setDice(randomDieValueArray())
+    }
   }
 
   useEffect(() => {
     const isWon = dice.every((die) => die.value === dice[0].value);
-    const allDiceHeld = dice.every((die) => die.isHeld === dice[0].isHeld);
+    const allDiceHeld = dice.every((die) => die.isHeld);
     if (isWon && allDiceHeld) {
       setGame((prevGame) => ({ ...prevGame, won: true }));
     } else setGame((prevGame) => ({ ...prevGame, won: false }));
   }, [dice]);
 
-  useEffect(() => {
-    console.log(game)
-    game.won && console.log(`Good job! You won in ${game.clicks} clicks!`)
-  }, [game])
-
   return (
     <main>
+      {game.won && (
+        <Confetti wind={0.002} gravity={0.025} numberOfPieces={200} />
+      )}
       <section className="title">
         <h1>Tenzies</h1>
         <h2>
           Roll until all dice are the same. Click each die to freeze it at its
           current value between rolls.
         </h2>
-        <h3>Count: {game.clicks}</h3>
+        <h3>{game.won ? `Good job! You won in ${game.clicks} clicks!` : `Count: ${game.clicks}`}</h3>
       </section>
       <ul className="die-wrapper">{diceElements}</ul>
-      <button onClick={roll}>Roll</button>
+      <button onClick={roll}>{game.won ? "New Game" : "Roll"}</button>
     </main>
   );
 }
